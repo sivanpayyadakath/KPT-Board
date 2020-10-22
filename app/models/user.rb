b73000548f3ba1ea043ed33811d3e197c1890301
sivanpayyadakath <sivan.payyadakath@monstar-lab.com>
2020-10-22 17:40:04 +0900

    seeds and login template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true;
-  validates_format_of :email, with: /^[a-zA-Z0-9._-]+@monstar-lab.com$/
+  validates_format_of :email, :with => /\A([a-zA-Z0-9._-]+@monstar-lab.com)\Z/i
   validates :password, presence: true, length: { minimum: 8, maximum: 20 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-movies = User.create(email: 'sivan.payyadakath@monstar-lab.com', password: '12345678')
+User.create!(email: 'sivan.payyadakath@monstar-lab.com', password: '12345678')
